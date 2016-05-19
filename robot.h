a1ac9c80025fa339e1749a2332e626425bfe31bf
C105-POSTE7 <C105-POSTE7@192.168.10.116>
2016-05-19 17:55:36 +0200

    RobotSte.DistanceTelemetre centre,gauche,droit

--- robot.h
+++ robot.h
@@ -10,54 +10,59 @@
 typedef struct robotStateBITS {
 
     union {
+
         struct {
             unsigned char taskEnCours;
             long timestampTaskStart;
             unsigned ModeAsservissement : 2;
             unsigned char matchState;
             unsigned char taskState;
-            
+
             //Asservissement en vitesse
-            double vitesseDroitFromOdometry;            
+            double vitesseDroitFromOdometry;
             double vitesseDroitConsigne;
             double vitesseDroitSortieCorrecteur;
-            
-            double vitesseGaucheFromOdometry;         
+
+            double vitesseGaucheFromOdometry;
             double vitesseGaucheConsigne;
             double vitesseGaucheSortieCorrecteur;
-                                  
-            double vitesseMoteur3Consigne;  
-            double vitesseMoteur3SortieCorrecteur;                    
+
+            double vitesseMoteur3Consigne;
+            double vitesseMoteur3SortieCorrecteur;
             double vitesseMoteur4Consigne;
             double vitesseMoteur4SortieCorrecteur;
-            
-            
+
+
 
             signed xPosTarget : 16;
             signed yPosTarget : 16;
             double angleRadianTarget;
-            
+
             unsigned LidarSensitivity : 8;
-            
+
             double xPosFromOdometry;
-            double yPosFromOdometry;            
+            double yPosFromOdometry;
             double xPosFromOdometry_1;
             double yPosFromOdometry_1;
             double angleRadianFromOdometry;
-            double angleRadianFromOdometry_1; 
-            
-            
-            
+            double angleRadianFromOdometry_1;
+
+
+
             signed xPosFromBalise : 16;
             signed yPosFromBalise : 16;
             signed angleRadianFromBalise : 16;
-            
+
             unsigned AligneAvantVersDirectionMouvement : 1;
-            
+
             double GainVitesseLineaire;
             double GainVitesseRotation;
             double VitesseLimiteRotation;
             double VitesseLimiteLineaire;
+
+            unsigned int distanceTelemetreCentre;
+            unsigned int distanceTelemetreGauche;
+            unsigned int distanceTelemetreDroit;
         };
     };
 } ROBOT_STATE_BITS;
