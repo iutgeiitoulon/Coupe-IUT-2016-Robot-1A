67aeafa975e0d55b9cd1493775472842607e21e7
C105-POSTE7 <C105-POSTE7@192.168.10.116>
2016-05-19 18:24:01 +0200

    include du main.h + if define robot_cachan + mise à jour des distance en robotState.distance

--- Telemetre_UltraSons.c
+++ Telemetre_UltraSons.c
@@ -5,78 +5,85 @@
 #include "events.h"
 #include "IO.h"
 #include "robot.h"
+#include "main.h"
 
+#if defined (ROBOT_EUROBOT)
 #define SEUIL_DETECTION_OBSTACLE 20
+#elif defined(ROBOT_CACHAN) 
+#define SEUIL_DETECTION_OBSTACLE 5
+#endif
 
-unsigned int distanceTelemetreDroit;
-unsigned int distanceTelemetreGauche;
-
+unsigned int seuilDetectionTelemetreCentre;
 unsigned int seuilDetectionTelemetreDroit;
 unsigned int seuilDetectionTelemetreGauche;
 
-void InitTelemetres(void) {
+void InitTelemetres(void)
+{
     //Configuration des télémètres
     //I2C1Write1(0xE0, RANGE_REG, 0x20); //Configuration du range maximal = value*43mm + 43mm
     I2C1Write1(0xE2, RANGE_REG, 0x20); //Configuration du range maximal = value*43mm + 43mm
     I2C1Write1(0xE4, RANGE_REG, 0x20); //Configuration du range maximal = value*43mm + 43mm
 }
 
-void SetUpAddressTelemetre(unsigned char address)
+void SetUpAddressTelemetre(unsigned char address) //attention reprogramme tout les capteurs !
 {
     I2C1Write1(ALL_I2C, CMD_REG, 0xA0); // cf. datasheet SRF08 page 7
-    I2C1Write1(ALL_I2C, CMD_REG, 0xAA); 
-    I2C1Write1(ALL_I2C, CMD_REG, 0xA5); 
-    I2C1Write1(ALL_I2C, CMD_REG, address); 
+    I2C1Write1(ALL_I2C, CMD_REG, 0xAA);
+    I2C1Write1(ALL_I2C, CMD_REG, 0xA5);
+    I2C1Write1(ALL_I2C, CMD_REG, address);
 }
 
-void StartNewUltrasonicMeasure() {
-    unsigned char data[10];                      
-    
+void StartNewUltrasonicMeasure()
+{
+    unsigned char data[10];
+
     //Cas Lecture et écriture synchrone
     I2C1ReadN(TELEMETRE_GAUCHE, CMD_REG, data, 10);
-    distanceTelemetreGauche = ((unsigned int) data[2])*256 + ((unsigned int) data[3]);
+    robotState.distanceTelemetreGauche = ((unsigned int) data[2])*256 + ((unsigned int) data[3]);
     I2C1ReadN(TELEMETRE_DROIT, CMD_REG, data, 10);
-    distanceTelemetreDroit = ((unsigned int) data[2])*256 + ((unsigned int) data[3]);
-    
-    I2C1Write1(ALL_I2C, CMD_REG, 0x51);  // Lecture en broadcast avec retour 
-                                         // distance en cm - cf datasheet page 4
-    
+    robotState.distanceTelemetreDroit = ((unsigned int) data[2])*256 + ((unsigned int) data[3]);
+
+    I2C1Write1(ALL_I2C, CMD_REG, 0x51); // Lecture en broadcast avec retour 
+    // distance en cm - cf datasheet page 4
+
     //Post-processing des distances ultrason mesurées
-    if(((distanceTelemetreGauche<SEUIL_DETECTION_OBSTACLE) || (distanceTelemetreDroit<SEUIL_DETECTION_OBSTACLE))&& sysEvents.DisableDetectionObstacle == 0)
+    if (((robotState.distanceTelemetreGauche < SEUIL_DETECTION_OBSTACLE) || (robotState.distanceTelemetreDroit < SEUIL_DETECTION_OBSTACLE)) && sysEvents.DisableDetectionObstacle == 0)
     {
+#if defined(ROBOT_EUROBOT)
         //On regarde si on n'est pas dans une zone d'exclusion
-        if((robotState.xPosFromOdometry < 500) && (robotState.xPosFromOdometry > -500))
+        if ((robotState.xPosFromOdometry < 500) && (robotState.xPosFromOdometry > -500))
         {
             //On ne déclenche pas : proche du chateau
         }
-        else
-            sysEvents.UltrasonicObjectDetectionEvent = 1;
+        //        else
+#endif
+        sysEvents.UltrasonicObjectDetectionEvent = 1;
     }
     else
     {
     }
-        
-        
+
+
 }
 
 void SendUltrasonicMeasure()
 {
     unsigned char payload[8];
-    int pos=0;
+    int pos = 0;
     payload[pos++] = (unsigned char) ((timestamp) >> 24);
     payload[pos++] = (unsigned char) ((timestamp) >> 16);
     payload[pos++] = (unsigned char) ((timestamp) >> 8);
     payload[pos++] = (unsigned char) ((timestamp) >> 0);
-    payload[pos++] = (unsigned char) (((int) distanceTelemetreGauche) >> 8);
-    payload[pos++] = (unsigned char) (((int) distanceTelemetreGauche) >> 0);
-    payload[pos++] = (unsigned char) (((int) distanceTelemetreDroit) >> 8);
-    payload[pos++] = (unsigned char) (((int) distanceTelemetreDroit) >> 0);
-    
+    payload[pos++] = (unsigned char) (((int) robotState.distanceTelemetreGauche) >> 8);
+    payload[pos++] = (unsigned char) (((int) robotState.distanceTelemetreGauche) >> 0);
+    payload[pos++] = (unsigned char) (((int) robotState.distanceTelemetreDroit) >> 8);
+    payload[pos++] = (unsigned char) (((int) robotState.distanceTelemetreDroit) >> 0);
+
     Uart1EncodeAndSendMessage(TELEMETRE_ULTRASONIC_DATA, pos, payload);
 }
 
-
-void InitSeuilDetectionSRF08(void) {    
+void InitSeuilDetectionSRF08(void)
+{
     seuilDetectionTelemetreGauche = 20;
     seuilDetectionTelemetreDroit = 20;
 }
