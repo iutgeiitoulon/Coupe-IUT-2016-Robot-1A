142f74a436cf44e67ddba4d4dd5d74131238922b
findlay <findlay@Tux>
2016-06-10 08:12:39 +0200

    ajout d'une fonction pour l'interface, wpecifique a A1

--- Robot.c
+++ Robot.c
@@ -7,15 +7,17 @@ void EnvoiAngleInterface()
 {
     unsigned char payload[8];
     int pos = 0;
-    payload[pos++] = 0;
-    payload[pos++] = 0;
-    payload[pos++] = 0;
-    payload[pos++] = 0;
-    payload[pos++] = 0;
-    payload[pos++] = 0;
+    payload[pos++] = robotState.distanceTelemetreGauche;
+    payload[pos++] = robotState.distanceTelemetreCentre;
+    payload[pos++] = robotState.vitesseGaucheConsigne;
+    payload[pos++] = robotState.vitesseDroitConsigne;
     payload[pos++] = (unsigned char) (((int) (robotState.AngleDegreFromBalise)) >> 8);
     payload[pos++] = (unsigned char) (((int) (robotState.AngleDegreFromBalise)) >> 0);
+    payload[pos++] = robotState.distanceTelemetreDroit;
+    payload[pos++] = 0;
 
     Uart1EncodeAndSendMessage(BALISE_ANGLES, pos, payload);
 }
+
+
 
