9d13f6662fdcde6d273c4f5e0d13b73962e6d25b
findlay <findlay@Tux>
2016-06-10 08:13:11 +0200

    suppression d'un appel inutile de la systemStateMachine

--- timer.c
+++ timer.c
@@ -264,7 +264,7 @@ void SetFreqTimer4(float freq)
 void __attribute__((interrupt, no_auto_psv)) _T4Interrupt(void)
 {
     IFS1bits.T4IF = 0;
-    SystemStateMachine();
+//    SystemStateMachine();
     
 //    LED_BLANCHE = !LED_BLANCHE;
 //    
